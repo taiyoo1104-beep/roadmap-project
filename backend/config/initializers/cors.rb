ccb8eb35efd467cc48c32fae6fcd724ce91f5dd4
taiyoo1104-beep <taiyoo1104@gmail.com>
2026-03-30 11:35:48 +0900

    環境不備修正

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,9 +1,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:5173" # React(Vite)のデフォルトURL
+    origins "http://localhost:3001" # React(Vite)のデフォルトURL
 
     resource "*",
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      expose: ['Authorization']
   end
 end
---
+++ frontend/app/login/page.tsx
@@ -0,0 +1,11 @@
+// frontend/src/app/login/page.tsx
+
+export default function LoginPage() {
+  return (
+    <>
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-blue-950 to-slate-900 p-4 text-white">
+
+      </main>
+    </>
+  );
+}
